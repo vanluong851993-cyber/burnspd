d1cc3b71f1f543555050d4cca83ca6d375144656
vanluong851993-cyber <vanluong851993@gmail.com>
2026-08-21 08:53:06 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/burbspd/',
+server: {
+port: 3000,
+host: '0.0.0.0',
+},
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
